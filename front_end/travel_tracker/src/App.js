bf3b08494d44085e991a4de84b6c7be2efb54673
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-12-08 11:19:16 -0500

    front end framework

---
+++ front_end/travel_tracker/src/App.js
@@ -0,0 +1,98 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  const [locations, setLocations] = useState([]);
+  const [newCountry, setNewCountry] = useState('');
+  const [newMajorCities, setNewMajorCities] = useState('');
+  const [newPhotos, setNewPhotos] = useState('');
+  const [newDate, setNewDate] = useState('');
+  const [newRecommend, setNewRecommend] = useState(true);
+
+
+
+
+//////// CRUD HANDLERS
+  
+const handleNewLocationFormSubmit = (event)=>{
+  event.preventDefault();
+  axios.post('http://localhost:3000/-travel',
+      {
+          country: newCountry,
+          majorCities: newMajorCities,
+          photos: newPhotos,
+          date: newDate,
+          recommend: newRecommend
+
+      }).then(()=>{axios.get('http://localhost:3000/-travel')
+          .then((response)=>{
+              setLocations(response.data)
+          })
+      })
+  };
+
+  const handleEdit = (event, travelData)=>{
+    event.preventDefault();
+    axios.put(`http://localhost:3000/-travel/${travelData._id}`,
+        {
+          country: newCountry,
+          majorCities: newMajorCities,
+          photos: newPhotos,
+          date: newDate,
+          recommend: newRecommend
+
+      }).then(()=>{
+            axios.get('http://localhost:3000/-travel').then((response)=>{
+                    setLocations(response.data)
+                })
+        })
+  };
+
+  const handleDelete = (travelData)=>{
+    axios.delete(`http://localhost:3000/-travel/${travelData._id}`).then(()=>{
+            axios.get('http://localhost:3000/-travel').then((response)=>{
+                    setLocations(response.data)
+                })
+        })
+  };
+
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/-travel').then((response)=>{
+          setLocations(response.data)
+        })
+      },[]);
+
+return (
+  <main>
+    <h1>Travel Tracker</h1>
+
+      <div>
+        <div>
+        <ul>
+          {locations.map((location)=>{
+            return (
+              <li>
+                <p>{location.country}</p>
+                <p>{location.majorCities}</p>
+                <p><img src={location.photos} /></p>
+                <p>{location.date}</p>
+                <p>{(location.recommend) ? <p>Recommend!</p> : <p>Not Recommended</p>} 
+                </p>
+              </li>
+                    )
+          })}
+          
+        </ul>  
+        </div>
+      </div>
+
+  </main>
+
+)
+
+
+};
+
+export default App;
